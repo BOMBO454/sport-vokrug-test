4f836c3a69a450446f8ee7c108b1db914faf4430
PSumbaev <p-sumbaev@yandex.ru>
2024-10-12 00:54:33 +0300

    add deploy workflow

--- src/apolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-
-export const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "https://beta.sosportom.ru/graphql/",
-  }),
-  cache: new InMemoryCache(),
-});
